2c70537fc8071c50004a85f3113a7ef4da464aad
RianVitor26 <rianvitorlhe@gmail.com>
2022-09-07 00:24:15 -0300

    feat: creating avatar

---
+++ links/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Avatar } from './components/avatar'
+
+function App() {
+  
+
+  return (
+    <>
+      <Avatar />
+    </>
+  )
+}
+
+export default App
---
+++ links/src/components/avatar/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import * as C from './style'
+
+export const Avatar = () => {
+  return (
+    <C.Container>
+      <C.Avatar></C.Avatar>
+    </C.Container>
+  )
+}
