83b330e30cc1dea37ed0ea9d1a0fe24b41d3eb73
RianVitor26 <rianvitorlhe@gmail.com>
2022-10-23 16:50:51 -0300

    feat: mudando link do protifolio

--- links/src/components/Links/index.jsx
+++ links/src/components/Links/index.jsx
@@ -6,7 +6,7 @@ export const Links = () => {
     <C.Container>
       <C.Link><a target="blank" href="https://github.com/RianVitor26">Github</a></C.Link>
       <C.Link><a target="blank" href="https://www.linkedin.com/in/rian-vitor-a036aa202/">Linkedin</a></C.Link>
-      <C.Link><a target="blank" href="https://rianvitor26.vercel.app/">Portifolio</a></C.Link>
+      <C.Link><a target="blank" href="https://rianvitor.vercel.app/">Portifolio</a></C.Link>
     </C.Container>
   )
 }
